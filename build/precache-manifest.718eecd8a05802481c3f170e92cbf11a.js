75a9895525f3bbefdcd5be24e76d7c4b1b7e9d7c
Sebastian Kondratowicz <feeverpl@gmail.com>
2019-04-14 23:53:22 +0200

    build added

---
+++ build/precache-manifest.718eecd8a05802481c3f170e92cbf11a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "f315a069ef9c048e26f3ed3798d3dbac",
+    "url": "./static/media/info-icon.f315a069.png"
+  },
+  {
+    "revision": "e0a4a16b00f57e2626c9b38bd0eaca78",
+    "url": "./static/media/img1.e0a4a16b.jpg"
+  },
+  {
+    "revision": "d01edd197a76ae000900f9f324dddd2c",
+    "url": "./static/media/arrow.d01edd19.png"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "a24d3cd89d4c04db9f03",
+    "url": "./static/js/main.f51d5bfa.chunk.js"
+  },
+  {
+    "revision": "47015ffc02bfcf5ad58a",
+    "url": "./static/js/2.000bb195.chunk.js"
+  },
+  {
+    "revision": "a24d3cd89d4c04db9f03",
+    "url": "./static/css/main.c3b15c44.chunk.css"
+  },
+  {
+    "revision": "94e0d3f44abb90da38632c4df40dbf8b",
+    "url": "./index.html"
+  }
+];
